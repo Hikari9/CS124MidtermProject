0f77612b08524c48dcf7ca856579c06f2a193f29
Rico Tiongson <thericotiongson@gmail.com>
2017-04-05 15:59:38 +0800

    Removed some unnecessary Spring modules

--- src/dragonsms/Main.java
+++ src/dragonsms/Main.java
@@ -5,6 +5,9 @@ public class Main {
     // Driver program
     public static void main(String[] args) {
 
+        // load Spring
+        SpringDriver.run();
+
         // create dragon server from System I/O
         DragonServer server = new DragonServer(System.in, System.out);
         System.out.println("Welcome to DragonSMS! Send HINT to get started!");
