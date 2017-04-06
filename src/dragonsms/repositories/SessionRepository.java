3b62e19da77310e393e1c323c777ff6f0dfee9ac
Rico Tiongson <thericotiongson@gmail.com>
2017-04-06 13:16:23 +0800

    This is the program for defense

--- src/dragonsms/repositories/SessionRepository.java
+++ src/dragonsms/repositories/SessionRepository.java
@@ -3,6 +3,8 @@ package dragonsms.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import dragonsms.entities.Session;
 
 @Repository
